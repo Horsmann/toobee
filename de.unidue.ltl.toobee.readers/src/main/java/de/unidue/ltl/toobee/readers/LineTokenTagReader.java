97c809c662724cec2cbf3e5ea121ab51d6351496
Tobias Horsmann <tobias.horsmann@gmail.com>
2016-05-02 09:43:56 +0200

    refactored reader to support sequence limit

--- de.unidue.ltl.toobee.readers/src/main/java/de/unidue/ltl/toobee/readers/LineTokenTagReader.java
+++ de.unidue.ltl.toobee.readers/src/main/java/de/unidue/ltl/toobee/readers/LineTokenTagReader.java
@@ -43,173 +43,198 @@ import de.tudarmstadt.ukp.dkpro.core.api.resources.MappingProvider;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 
-@TypeCapability(outputs = {
-		"de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData",
-		"de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence",
-		"de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token",
-		"de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS" })
-public class LineTokenTagReader extends
-		JCasResourceCollectionReader_ImplBase {
+@TypeCapability(outputs = { "de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData",
+        "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence",
+        "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token",
+        "de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS" })
+public class LineTokenTagReader
+    extends JCasResourceCollectionReader_ImplBase
+{
 
-	public static final String PARAM_POS_MAPPING_LOCATION = ComponentParameters.PARAM_POS_MAPPING_LOCATION;
-	@ConfigurationParameter(name = PARAM_POS_MAPPING_LOCATION, mandatory = false)
-	protected String mappingPosLocation;
+    public static final String PARAM_POS_MAPPING_LOCATION = ComponentParameters.PARAM_POS_MAPPING_LOCATION;
+    @ConfigurationParameter(name = PARAM_POS_MAPPING_LOCATION, mandatory = false)
+    protected String mappingPosLocation;
 
-	public static final String PARAM_POS_TAGSET = ComponentParameters.PARAM_POS_TAG_SET;
-	@ConfigurationParameter(name = PARAM_POS_TAGSET, mandatory = false)
-	protected String posTagset;
+    public static final String PARAM_POS_TAGSET = ComponentParameters.PARAM_POS_TAG_SET;
+    @ConfigurationParameter(name = PARAM_POS_TAGSET, mandatory = false)
+    protected String posTagset;
 
-	public static final String PARAM_SOURCE_ENCODING = ComponentParameters.PARAM_SOURCE_ENCODING;
-	@ConfigurationParameter(name = PARAM_SOURCE_ENCODING, mandatory = true, defaultValue = "UTF-8")
-	protected String encoding;
+    public static final String PARAM_SOURCE_ENCODING = ComponentParameters.PARAM_SOURCE_ENCODING;
+    @ConfigurationParameter(name = PARAM_SOURCE_ENCODING, mandatory = true, defaultValue = "UTF-8")
+    protected String encoding;
 
-	public static final String PARAM_LANGUAGE = ComponentParameters.PARAM_LANGUAGE;
-	@ConfigurationParameter(name = PARAM_LANGUAGE, mandatory = false)
-	private String language;
+    public static final String PARAM_LANGUAGE = ComponentParameters.PARAM_LANGUAGE;
+    @ConfigurationParameter(name = PARAM_LANGUAGE, mandatory = false)
+    private String language;
 
-	public static final String ENCODING_AUTO = "auto";
+    public static final String PARAM_SEQUENCES_PER_CAS = "PARAM_SEQUENCES_PER_CAS";
+    @ConfigurationParameter(name = PARAM_SEQUENCES_PER_CAS, mandatory = true, defaultValue = "100")
+    private int sentLimit;
 
-	private MappingProvider posMappingProvider;
+    public static final String ENCODING_AUTO = "auto";
 
-	private BufferedReader br;
+    private MappingProvider posMappingProvider;
 
-	private List<String> posting = new ArrayList<String>();
+    private BufferedReader br;
 
-	private List<BufferedReader> bfs = new ArrayList<BufferedReader>();
-	private int currentReader = 0;
+    private List<List<String>> sequences = new ArrayList<List<String>>();
 
-	private int instanceId = 1;
+    private List<BufferedReader> bfs = new ArrayList<BufferedReader>();
+    private int currentReader = 0;
 
-	@Override
-	public void initialize(UimaContext context)
-			throws ResourceInitializationException {
-		super.initialize(context);
+    private int instanceId = 1;
 
-		posMappingProvider = new MappingProvider();
-		posMappingProvider
-				.setDefault(
-						MappingProvider.LOCATION,
-						"classpath:/de/tudarmstadt/ukp/dkpro/"
-								+ "core/api/lexmorph/tagset/${language}-${tagger.tagset}-pos.map");
-		posMappingProvider.setDefault(MappingProvider.BASE_TYPE,
-				POS.class.getName());
-		posMappingProvider.setDefault("tagger.tagset", "default");
-		posMappingProvider.setOverride(MappingProvider.LOCATION,
-				mappingPosLocation);
-		posMappingProvider.setOverride(MappingProvider.LANGUAGE, language);
-		posMappingProvider.setOverride("tagger.tagset", posTagset);
+    @Override
+    public void initialize(UimaContext context)
+        throws ResourceInitializationException
+    {
+        super.initialize(context);
 
-		try {
-			for (Resource r : getResources()) {
-				String name = r.getResource().getFile().getName();
-				InputStreamReader is = null;
-				if (name.endsWith(".gz")) {
-					is = new InputStreamReader(new GZIPInputStream(
-							r.getInputStream()), encoding);
-				} else {
-					is = new InputStreamReader(r.getInputStream(), encoding);
-				}
-				br = new BufferedReader(is);
-				bfs.add(br);
-			}
-		} catch (Exception e) {
-			throw new IllegalStateException(e);
-		}
-	}
+        posMappingProvider = new MappingProvider();
+        posMappingProvider.setDefault(MappingProvider.LOCATION,
+                "classpath:/de/tudarmstadt/ukp/dkpro/"
+                        + "core/api/lexmorph/tagset/${language}-${tagger.tagset}-pos.map");
+        posMappingProvider.setDefault(MappingProvider.BASE_TYPE, POS.class.getName());
+        posMappingProvider.setDefault("tagger.tagset", "default");
+        posMappingProvider.setOverride(MappingProvider.LOCATION, mappingPosLocation);
+        posMappingProvider.setOverride(MappingProvider.LANGUAGE, language);
+        posMappingProvider.setOverride("tagger.tagset", posTagset);
 
-	public void getNext(JCas aJCas) throws IOException, CollectionException {
+        try {
+            for (Resource r : getResources()) {
+                String name = r.getResource().getFile().getName();
+                InputStreamReader is = null;
+                if (name.endsWith(".gz")) {
+                    is = new InputStreamReader(new GZIPInputStream(r.getInputStream()), encoding);
+                }
+                else {
+                    is = new InputStreamReader(r.getInputStream(), encoding);
+                }
+                br = new BufferedReader(is);
+                bfs.add(br);
+            }
+        }
+        catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
 
-		DocumentMetaData md = new DocumentMetaData(aJCas);
-		md.setDocumentTitle("");
-		md.setDocumentId("" + (instanceId++));
-		md.setLanguage(language);
-		md.addToIndexes();
+    public void getNext(JCas aJCas)
+        throws IOException, CollectionException
+    {
 
-		try {
-			posMappingProvider.configure(aJCas.getCas());
-		} catch (AnalysisEngineProcessException e1) {
-			throw new CollectionException(e1);
-		}
+        DocumentMetaData md = new DocumentMetaData(aJCas);
+        md.setDocumentTitle("");
+        md.setDocumentId("" + (instanceId++));
+        md.setLanguage(language);
+        md.addToIndexes();
 
-		String documentText = "";
-		for (int i = 0; i < posting.size(); i++) {
+        try {
+            posMappingProvider.configure(aJCas.getCas());
+        }
+        catch (AnalysisEngineProcessException e1) {
+            throw new CollectionException(e1);
+        }
 
-			// There might a case where trailing spaces exist - make sure that
-			// we find only one blank!
-			String pairs = posting.get(i).replaceAll(" +", " ");
+        StringBuilder documentText = new StringBuilder();
 
-			String[] tokenTag = pairs.split(" ");
+        int seqStart = 0;
+        for (int k = 0; k < sequences.size(); k++) {
 
-			int tokenLen = tokenTag[0].length();
-			documentText += tokenTag[0];
+            List<String> sequence = sequences.get(k);
 
-			int tokStart = documentText.length() - tokenLen;
-			int tokEnd = documentText.length();
-			Token token = new Token(aJCas, tokStart, tokEnd);
-			token.addToIndexes();
+            for (int i = 0; i < sequence.size(); i++) {
+                String pairs = sequence.get(i).replaceAll(" +", " ");
 
-			if (i + 1 < posting.size()) {
-				documentText += " ";
-			}
+                String[] tokenTag = pairs.split(" ");
 
-			Type posTag = posMappingProvider.getTagType(tokenTag[1]);
-			POS pos = (POS) aJCas.getCas().createAnnotation(posTag,
-					token.getBegin(), token.getEnd());
-			pos.setPosValue(tokenTag[1]);
-			pos.addToIndexes();
-			token.setPos(pos);
+                int tokenLen = tokenTag[0].length();
+                documentText.append(tokenTag[0]);
 
-		}
-		aJCas.setDocumentText(documentText);
-		
-		Sentence sentence = new Sentence(aJCas, 0, aJCas.getDocumentText()
-				.length());
-		sentence.addToIndexes();
+                int tokStart = documentText.length() - tokenLen;
+                int tokEnd = documentText.length();
+                Token token = new Token(aJCas, tokStart, tokEnd);
+                token.addToIndexes();
 
+                if (i + 1 < sequence.size()) {
+                    documentText.append(" ");
+                }
 
-	}
+                Type posTag = posMappingProvider.getTagType(tokenTag[1]);
+                POS pos = (POS) aJCas.getCas().createAnnotation(posTag, token.getBegin(),
+                        token.getEnd());
+                pos.setPosValue(tokenTag[1]);
+                pos.addToIndexes();
+                token.setPos(pos);
+            }
+            Sentence sentence = new Sentence(aJCas, seqStart, documentText.length());
+            sentence.addToIndexes();
 
+            if (k + 1 < sequences.size()) {
+                documentText.append(" ");
+            }
+            seqStart = documentText.length();
 
+        }
+        aJCas.setDocumentText(documentText.toString());
+    }
 
-	public boolean hasNext() throws IOException, CollectionException {
-		BufferedReader br = getBufferedReader();
+    public boolean hasNext()
+        throws IOException, CollectionException
+    {
+        BufferedReader br = getBufferedReader();
 
-		posting = new ArrayList<String>();
-		String readLine = null;
-		while ((readLine = br.readLine()) != null) {
-			if (readLine.isEmpty()) {
-				break;
-			}
-			posting.add(readLine);
-		}
-		if (!posting.isEmpty()) {
-			return true;
-		}
+        int readSeq = 0;
+        sequences = new ArrayList<List<String>>();
+        List<String> sequence = new ArrayList<>();
+        String readLine = null;
+        while ((readLine = br.readLine()) != null) {
+            if (readLine.isEmpty()) {
+                readSeq++;
+                if (readSeq == sentLimit) {
+                    break;
+                }
+                sequences.add(sequence);
+                sequence = new ArrayList<>();
+                continue;
+            }
+            sequence.add(readLine);
+        }
+        if (!sequence.isEmpty()) {
+            sequences.add(sequence);
+        }
+        if (!sequences.isEmpty()) {
+            return true;
+        }
 
-		return closeReaderOpenNext();
+        return closeReaderOpenNext();
 
-	}
+    }
 
-	private boolean closeReaderOpenNext() throws CollectionException,
-			IOException {
-		try {
-			bfs.get(currentReader).close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    private boolean closeReaderOpenNext()
+        throws CollectionException, IOException
+    {
+        try {
+            bfs.get(currentReader).close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		if (currentReader + 1 < bfs.size()) {
-			currentReader++;
-			return hasNext();
-		}
-		return false;
-	}
+        if (currentReader + 1 < bfs.size()) {
+            currentReader++;
+            return hasNext();
+        }
+        return false;
+    }
 
-	private BufferedReader getBufferedReader() {
-		return bfs.get(currentReader);
-	}
+    private BufferedReader getBufferedReader()
+    {
+        return bfs.get(currentReader);
+    }
 
-	public Progress[] getProgress() {
-		return null;
-	}
+    public Progress[] getProgress()
+    {
+        return null;
+    }
 }
--- de.unidue.ltl.toobee.readers/src/test/java/de/unidue/ltl/toobee/readers/TestLineTokenTagReader.java
+++ de.unidue.ltl.toobee.readers/src/test/java/de/unidue/ltl/toobee/readers/TestLineTokenTagReader.java
@@ -3,8 +3,6 @@ package de.unidue.ltl.toobee.readers;
 import static org.apache.uima.fit.util.JCasUtil.select;
 import static org.junit.Assert.assertEquals;
 
-import java.util.Collection;
-
 import org.apache.uima.collection.CollectionReader;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.factory.JCasFactory;
@@ -14,61 +12,109 @@ import org.apache.uima.resource.ResourceInitializationException;
 import org.junit.Test;
 
 import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.core.testing.AssertAnnotations;
 
-public class TestLineTokenTagReader {
+public class TestLineTokenTagReader
+{
 
-	@Test
-	public void testTokenCounts() throws Exception {
+    @Test
+    public void testAnnotationCounts()
+        throws Exception
+    {
 
-		CollectionReader reader = getReader("*.txt");
+        CollectionReader reader = getReader("*.txt");
 
-		int tokenTotal = 0;
-		while (reader.hasNext()) {
-			JCas jcas = JCasFactory.createJCas();
-			reader.getNext(jcas.getCas());
-			Collection<Token> tokens = JCasUtil.select(jcas, Token.class);
-			tokenTotal += tokens.size();
-		}
+        int tokenTotal = 0;
+        int seqTotal = 0;
+        while (reader.hasNext()) {
+            JCas jcas = JCasFactory.createJCas();
+            reader.getNext(jcas.getCas());
+            tokenTotal += JCasUtil.select(jcas, Token.class).size();
+            seqTotal += JCasUtil.select(jcas, Sentence.class).size();
+        }
 
-		assertEquals(53, tokenTotal);
-	}
+        assertEquals(53, tokenTotal);
+        assertEquals(4, seqTotal);
+    }
 
-	@Test
-	public void testPosTagsOfFirstSequence() throws Exception {
-		CollectionReader reader = getReader("input1.txt");
-		JCas jcas = JCasFactory.createJCas();
-		reader.hasNext();
-		reader.getNext(jcas.getCas());
+    @Test
+    public void testLimit()
+        throws Exception
+    {
+        CollectionReader reader = getReader("*.limit", 5);
 
-		String[] expectedFine = new String[] { "JJ", "NNP", "NNP", "NNP", "CD", "VBZ", "CD", "NN", "NN", "NNP", "NNP",
-				":", "DT", "DT", "NN", "NN", "VBZ", "NNP", "URL", "HT" };
+        JCas jcas = JCasFactory.createJCas();
+        reader.hasNext();
+        reader.getNext(jcas.getCas());
+        assertEquals(5, JCasUtil.select(jcas, Sentence.class).size());
+        assertEquals(41, JCasUtil.select(jcas, Token.class).size());
 
-		String[] expectedCoarse = new String[] { "ADJ", "NP", "NP", "NP", "CARD", "V", "CARD", "NN", "NN", "NP", "NP",
-				"PUNC", "ART", "ART", "NN", "NN", "V", "NP", "O", "O" };
+        jcas = JCasFactory.createJCas();
+        reader.hasNext();
+        reader.getNext(jcas.getCas());
+        assertEquals(1, JCasUtil.select(jcas, Sentence.class).size());
+        assertEquals(7, JCasUtil.select(jcas, Token.class).size());
+    }
 
-		AssertAnnotations.assertPOS(expectedCoarse, expectedFine, select(jcas, POS.class));
-	}
+    @Test
+    public void testPosTags()
+        throws Exception
+    {
+        CollectionReader reader = getReader("input1.txt");
+        JCas jcas = JCasFactory.createJCas();
+        reader.hasNext();
+        reader.getNext(jcas.getCas());
 
-	@Test
-	public void testTokenBoundariesFirstSentence() throws Exception {
-		CollectionReader reader = getReader("input1.txt");
-		JCas jcas = JCasFactory.createJCas();
-		reader.hasNext();
-		reader.getNext(jcas.getCas());
+        String[] expectedFine = new String[] { "JJ", "NNP", "NNP", "NNP", "CD", "VBZ", "CD", "NN",
+                "NN", "NNP", "NNP", ":", "DT", "DT", "NN", "NN", "VBZ", "NNP", "URL", "HT", "USR",
+                "PRP", "VBP", "DT", "VBZ", "WRB", "PRP", "VBP", "VBG", "VB", "RB" };
 
-		String[] tokens = new String[] { "Small", "Biz", "Tech", "Tour", "2010", "Launches", "Five", "City", "Tour",
-				"MONTCLAIR", "N.J.", "...:", "The", "all", "day", "event", "features", "America's...",
-				"http://tinyurl.com/28hd9fu", "#fb" };
+        String[] expectedCoarse = new String[] { "ADJ", "NP", "NP", "NP", "CARD", "V", "CARD",
+                "NN", "NN", "NP", "NP", "PUNC", "ART", "ART", "NN", "NN", "V", "NP", "O", "O", "O",
+                "PR", "V", "ART", "V", "ADV", "PR", "V", "V", "V", "ADV" };
 
-		AssertAnnotations.assertToken(tokens, select(jcas, Token.class));
-	}
+        AssertAnnotations.assertPOS(expectedCoarse, expectedFine, select(jcas, POS.class));
+    }
 
-	private CollectionReader getReader(String pattern) throws ResourceInitializationException {
-		return CollectionReaderFactory.createReader(LineTokenTagReader.class, LineTokenTagReader.PARAM_LANGUAGE, "en",
-				LineTokenTagReader.PARAM_SOURCE_LOCATION, "src/test/resources/LineTokenTag/",
-				LineTokenTagReader.PARAM_POS_TAGSET, "ptb", LineTokenTagReader.PARAM_PATTERNS, pattern);
-	}
+    @Test
+    public void testTokenBoundaries()
+        throws Exception
+    {
+        CollectionReader reader = getReader("input1.txt");
+        JCas jcas = JCasFactory.createJCas();
+        reader.hasNext();
+        reader.getNext(jcas.getCas());
+
+        String[] tokens = new String[] { "Small", "Biz", "Tech", "Tour", "2010", "Launches",
+                "Five", "City", "Tour", "MONTCLAIR", "N.J.", "...:", "The", "all", "day", "event",
+                "features", "America's...", "http://tinyurl.com/28hd9fu", "#fb", "@MiSS_SOTO", "I",
+                "think", "that", "'s", "when", "I", "'m", "gonna", "be", "there" };
+
+        String [] sentences = new String [] {"Small Biz Tech Tour 2010 Launches Five City Tour MONTCLAIR N.J. ...: The all day event features America's... http://tinyurl.com/28hd9fu #fb", "@MiSS_SOTO I think that 's when I 'm gonna be there"};
+        
+        AssertAnnotations.assertToken(tokens, select(jcas, Token.class));
+        AssertAnnotations.assertSentence(sentences, select(jcas, Sentence.class));
+    }
+
+    private CollectionReader getReader(String pattern)
+        throws ResourceInitializationException
+    {
+        return CollectionReaderFactory.createReader(LineTokenTagReader.class,
+                LineTokenTagReader.PARAM_LANGUAGE, "en", LineTokenTagReader.PARAM_SOURCE_LOCATION,
+                "src/test/resources/LineTokenTag/", LineTokenTagReader.PARAM_POS_TAGSET, "ptb",
+                LineTokenTagReader.PARAM_PATTERNS, pattern);
+    }
+
+    private CollectionReader getReader(String pattern, int limit)
+        throws ResourceInitializationException
+    {
+        return CollectionReaderFactory.createReader(LineTokenTagReader.class,
+                LineTokenTagReader.PARAM_LANGUAGE, "en", LineTokenTagReader.PARAM_SOURCE_LOCATION,
+                "src/test/resources/LineTokenTag/", LineTokenTagReader.PARAM_POS_TAGSET, "ptb",
+                LineTokenTagReader.PARAM_PATTERNS, pattern,
+                LineTokenTagReader.PARAM_SEQUENCES_PER_CAS, limit);
+    }
 
 }
