4044d98958c2a342ee4413b71d378181ca3bc07a
Tobias Horsmann <tobias.horsmann@gmail.com>
2016-06-03 10:49:59 +0200

    added superclass and interface to feature

--- de.unidue.ltl.toobee.features/src/main/java/de/unidue/ltl/toobee/feature/is/IsQuote.java
+++ de.unidue.ltl.toobee.features/src/main/java/de/unidue/ltl/toobee/feature/is/IsQuote.java
@@ -5,16 +5,20 @@ import java.util.Set;
 
 import org.apache.uima.jcas.JCas;
 import org.dkpro.tc.api.exception.TextClassificationException;
+import org.dkpro.tc.api.features.ClassificationUnitFeatureExtractor;
 import org.dkpro.tc.api.features.Feature;
+import org.dkpro.tc.api.features.FeatureExtractorResource_ImplBase;
 import org.dkpro.tc.api.type.TextClassificationUnit;
 
 public class IsQuote
+    extends FeatureExtractorResource_ImplBase
+    implements ClassificationUnitFeatureExtractor
 {
     private final String FEATURE_NAME = "isQuote";
 
-    public Set<Feature> extract(JCas aView,
-            TextClassificationUnit aClassificationUnit)
-            throws TextClassificationException {
+    public Set<Feature> extract(JCas aView, TextClassificationUnit aClassificationUnit)
+        throws TextClassificationException
+    {
 
         boolean b = is(aClassificationUnit.getCoveredText());
         Feature feature = new Feature(FEATURE_NAME, b ? 1 : 0);
@@ -24,7 +28,8 @@ public class IsQuote
         return features;
     }
 
-    static boolean is(String aToken) {
+    static boolean is(String aToken)
+    {
         return aToken.equals("\"") || aToken.equals(",,") || aToken.equals("`");
     }
 
