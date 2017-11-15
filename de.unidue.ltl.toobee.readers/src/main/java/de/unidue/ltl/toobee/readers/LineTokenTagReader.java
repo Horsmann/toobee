df613826a463a60480ac330f51449c2b372836ee
Tobias Horsmann <tobias.horsmann@gmail.com>
2017-11-15 08:52:20 +0100

    refactoring

--- de.unidue.ltl.toobee.readers/src/main/java/de/unidue/ltl/toobee/readers/LineTokenTagReader.java
+++ de.unidue.ltl.toobee.readers/src/main/java/de/unidue/ltl/toobee/readers/LineTokenTagReader.java
@@ -44,220 +44,206 @@ import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 
 @TypeCapability(outputs = { "de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData",
-        "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence",
-        "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token",
-        "de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS" })
-public class LineTokenTagReader
-    extends JCasResourceCollectionReader_ImplBase
-{
+		"de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence",
+		"de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token",
+		"de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS" })
+public class LineTokenTagReader extends JCasResourceCollectionReader_ImplBase {
 
-    public static final String PARAM_POS_MAPPING_LOCATION = ComponentParameters.PARAM_POS_MAPPING_LOCATION;
-    @ConfigurationParameter(name = PARAM_POS_MAPPING_LOCATION, mandatory = false)
-    protected String mappingPosLocation;
+	public static final String PARAM_POS_MAPPING_LOCATION = ComponentParameters.PARAM_POS_MAPPING_LOCATION;
+	@ConfigurationParameter(name = PARAM_POS_MAPPING_LOCATION, mandatory = false)
+	protected String mappingPosLocation;
 
-    public static final String PARAM_POS_TAGSET = ComponentParameters.PARAM_POS_TAG_SET;
-    @ConfigurationParameter(name = PARAM_POS_TAGSET, mandatory = false)
-    protected String posTagset;
+	public static final String PARAM_POS_TAGSET = ComponentParameters.PARAM_POS_TAG_SET;
+	@ConfigurationParameter(name = PARAM_POS_TAGSET, mandatory = false)
+	protected String posTagset;
 
-    public static final String PARAM_SOURCE_ENCODING = ComponentParameters.PARAM_SOURCE_ENCODING;
-    @ConfigurationParameter(name = PARAM_SOURCE_ENCODING, mandatory = true, defaultValue = "UTF-8")
-    protected String encoding;
+	public static final String PARAM_SOURCE_ENCODING = ComponentParameters.PARAM_SOURCE_ENCODING;
+	@ConfigurationParameter(name = PARAM_SOURCE_ENCODING, mandatory = true, defaultValue = "UTF-8")
+	protected String encoding;
 
-    public static final String PARAM_LANGUAGE = ComponentParameters.PARAM_LANGUAGE;
-    @ConfigurationParameter(name = PARAM_LANGUAGE, mandatory = false)
-    private String language;
+	public static final String PARAM_LANGUAGE = ComponentParameters.PARAM_LANGUAGE;
+	@ConfigurationParameter(name = PARAM_LANGUAGE, mandatory = false)
+	private String language;
 
-    public static final String PARAM_LOWER_CASE = "PARAM_LOWER_CASE";
-    @ConfigurationParameter(name = PARAM_LOWER_CASE, mandatory = false, defaultValue = "false")
-    private boolean lowerCase;
+	public static final String PARAM_LOWER_CASE = "PARAM_LOWER_CASE";
+	@ConfigurationParameter(name = PARAM_LOWER_CASE, mandatory = false, defaultValue = "false")
+	private boolean lowerCase;
 
-    public static final String PARAM_SEPARATOR = "PARAM_SEPARATOR";
-    @ConfigurationParameter(name = PARAM_SEPARATOR, mandatory = true, defaultValue = " ")
-    private String separator;
+	public static final String PARAM_SEPARATOR = "PARAM_SEPARATOR";
+	@ConfigurationParameter(name = PARAM_SEPARATOR, mandatory = true, defaultValue = " ")
+	private String separator;
 
-    public static final String PARAM_SET_POS = "PARAM_SET_POS";
-    @ConfigurationParameter(name = PARAM_SET_POS, mandatory = true, defaultValue = "true")
-    private boolean setPos;
+	public static final String PARAM_SET_POS = "PARAM_SET_POS";
+	@ConfigurationParameter(name = PARAM_SET_POS, mandatory = true, defaultValue = "true")
+	private boolean setPos;
 
-    public static final String PARAM_SEQUENCES_PER_CAS = "PARAM_SEQUENCES_PER_CAS";
-    @ConfigurationParameter(name = PARAM_SEQUENCES_PER_CAS, mandatory = true, defaultValue = "1000")
-    private String seqLimitString;
+	public static final String PARAM_SEQUENCES_PER_CAS = "PARAM_SEQUENCES_PER_CAS";
+	@ConfigurationParameter(name = PARAM_SEQUENCES_PER_CAS, mandatory = true, defaultValue = "1000")
+	private String seqLimitString;
 
-    int seqLimit=-1;
-    
-    public static final String ENCODING_AUTO = "auto";
+	int seqLimit = -1;
 
-    private MappingProvider posMappingProvider;
+	public static final String ENCODING_AUTO = "auto";
 
-    private BufferedReader br;
+	private MappingProvider posMappingProvider;
 
-    private List<List<String>> sequences = new ArrayList<List<String>>();
+	private BufferedReader reader;
 
-    private List<BufferedReader> bfs = new ArrayList<BufferedReader>();
-    private int currentReader = 0;
+	private List<List<String>> sequences = new ArrayList<List<String>>();
 
-    private int instanceId = 1;
+	private List<Resource> resourcePool = new ArrayList<Resource>();
+	private int pointer = -1;
 
-    @Override
-    public void initialize(UimaContext context)
-        throws ResourceInitializationException
-    {
-        super.initialize(context);
-        
-        seqLimit = Integer.valueOf(seqLimitString);
+	private int instanceId = 1;
 
-        posMappingProvider = new MappingProvider();
-        posMappingProvider.setDefault(MappingProvider.LOCATION,
-                "classpath:/de/tudarmstadt/ukp/dkpro/"
-                        + "core/api/lexmorph/tagset/${language}-${tagger.tagset}-pos.map");
-        posMappingProvider.setDefault(MappingProvider.BASE_TYPE, POS.class.getName());
-        posMappingProvider.setDefault("tagger.tagset", "default");
-        posMappingProvider.setOverride(MappingProvider.LOCATION, mappingPosLocation);
-        posMappingProvider.setOverride(MappingProvider.LANGUAGE, language);
-        posMappingProvider.setOverride("tagger.tagset", posTagset);
+	@Override
+	public void initialize(UimaContext context) throws ResourceInitializationException {
+		super.initialize(context);
 
-        try {
-            for (Resource r : getResources()) {
-                String name = r.getResource().getFile().getName();
-                InputStreamReader is = null;
-                if (name.endsWith(".gz")) {
-                    is = new InputStreamReader(new GZIPInputStream(r.getInputStream()), encoding);
-                }
-                else {
-                    is = new InputStreamReader(r.getInputStream(), encoding);
-                }
-                br = new BufferedReader(is);
-                bfs.add(br);
-            }
-        }
-        catch (Exception e) {
-            throw new IllegalStateException(e);
-        }
-    }
+		seqLimit = Integer.valueOf(seqLimitString);
 
-    public void getNext(JCas aJCas)
-        throws IOException, CollectionException
-    {
+		posMappingProvider = new MappingProvider();
+		posMappingProvider.setDefault(MappingProvider.LOCATION, "classpath:/de/tudarmstadt/ukp/dkpro/"
+				+ "core/api/lexmorph/tagset/${language}-${tagger.tagset}-pos.map");
+		posMappingProvider.setDefault(MappingProvider.BASE_TYPE, POS.class.getName());
+		posMappingProvider.setDefault("tagger.tagset", "default");
+		posMappingProvider.setOverride(MappingProvider.LOCATION, mappingPosLocation);
+		posMappingProvider.setOverride(MappingProvider.LANGUAGE, language);
+		posMappingProvider.setOverride("tagger.tagset", posTagset);
 
-        DocumentMetaData md = new DocumentMetaData(aJCas);
-        md.setDocumentTitle("");
-        md.setDocumentId("" + (instanceId++));
-        md.setLanguage(language);
-        md.addToIndexes();
+		List<Resource> resourcess = new ArrayList<Resource>(getResources());
+		for (int i = 1; i < resourcess.size(); i++) {
+			resourcePool.add(resourcess.get(i));
+		}
+		try {
+			reader = new BufferedReader(openStream(resourcess.get(0)));
+		} catch (IOException e) {
+			throw new ResourceInitializationException(e);
+		}
+	}
 
-        try {
-            posMappingProvider.configure(aJCas.getCas());
-        }
-        catch (AnalysisEngineProcessException e1) {
-            throw new CollectionException(e1);
-        }
+	public void getNext(JCas aJCas) throws IOException, CollectionException {
 
-        StringBuilder documentText = new StringBuilder();
+		DocumentMetaData md = new DocumentMetaData(aJCas);
+		md.setDocumentTitle("");
+		md.setDocumentId("" + (instanceId++));
+		md.setLanguage(language);
+		md.addToIndexes();
 
-        int seqStart = 0;
-        for (int k = 0; k < sequences.size(); k++) {
+		try {
+			posMappingProvider.configure(aJCas.getCas());
+		} catch (AnalysisEngineProcessException e1) {
+			throw new CollectionException(e1);
+		}
 
-            List<String> sequence = sequences.get(k);
+		StringBuilder documentText = new StringBuilder();
 
-            for (int i = 0; i < sequence.size(); i++) {
-                String pairs = sequence.get(i).replaceAll(" +", " ");
+		int seqStart = 0;
+		for (int k = 0; k < sequences.size(); k++) {
 
-                int idxLastSpace = pairs.lastIndexOf(separator);
-                String token = pairs.substring(0, idxLastSpace);
-                String tag = pairs.substring(idxLastSpace + 1);
+			List<String> sequence = sequences.get(k);
 
-                int tokenLen = token.length();
-                if (lowerCase) {
-                    token = token.toLowerCase();
-                }
-                documentText.append(token);
+			for (int i = 0; i < sequence.size(); i++) {
+				String pairs = sequence.get(i).replaceAll(" +", " ");
 
-                int tokStart = documentText.length() - tokenLen;
-                int tokEnd = documentText.length();
-                Token t = new Token(aJCas, tokStart, tokEnd);
-                t.addToIndexes();
+				int idxLastSpace = pairs.lastIndexOf(separator);
+				String token = pairs.substring(0, idxLastSpace);
+				String tag = pairs.substring(idxLastSpace + 1);
 
-                if (i + 1 < sequence.size()) {
-                    documentText.append(" ");
-                }
+				int tokenLen = token.length();
+				if (lowerCase) {
+					token = token.toLowerCase();
+				}
+				documentText.append(token);
 
-                if (setPos) {
-                    Type posTag = posMappingProvider.getTagType(tag);
-                    POS pos = (POS) aJCas.getCas().createAnnotation(posTag, t.getBegin(),
-                            t.getEnd());
-                    pos.setPosValue(tag);
-                    pos.addToIndexes();
-                    t.setPos(pos);
-                }
-            }
-            Sentence sentence = new Sentence(aJCas, seqStart, documentText.length());
-            sentence.addToIndexes();
+				int tokStart = documentText.length() - tokenLen;
+				int tokEnd = documentText.length();
+				Token t = new Token(aJCas, tokStart, tokEnd);
+				t.addToIndexes();
 
-            if (k + 1 < sequences.size()) {
-                documentText.append(" ");
-            }
-            seqStart = documentText.length();
+				if (i + 1 < sequence.size()) {
+					documentText.append(" ");
+				}
 
-        }
-        aJCas.setDocumentText(documentText.toString());
-    }
+				if (setPos) {
+					Type posTag = posMappingProvider.getTagType(tag);
+					POS pos = (POS) aJCas.getCas().createAnnotation(posTag, t.getBegin(), t.getEnd());
+					pos.setPosValue(tag);
+					pos.addToIndexes();
+					t.setPos(pos);
+				}
+			}
+			Sentence sentence = new Sentence(aJCas, seqStart, documentText.length());
+			sentence.addToIndexes();
 
-    public boolean hasNext()
-        throws IOException, CollectionException
-    {
-        BufferedReader br = getBufferedReader();
+			if (k + 1 < sequences.size()) {
+				documentText.append(" ");
+			}
+			seqStart = documentText.length();
 
-        int readSeq = 0;
-        sequences = new ArrayList<List<String>>();
-        List<String> sequence = new ArrayList<>();
-        String readLine = null;
-        while ((readLine = br.readLine()) != null) {
-            if (readLine.isEmpty()) {
-                readSeq++;
-                if (readSeq == seqLimit) {
-                    break;
-                }
-                sequences.add(sequence);
-                sequence = new ArrayList<>();
-                continue;
-            }
-            sequence.add(readLine);
-        }
-        if (!sequence.isEmpty()) {
-            sequences.add(sequence);
-        }
-        if (!sequences.isEmpty()) {
-            return true;
-        }
+		}
+		aJCas.setDocumentText(documentText.toString());
+	}
 
-        return closeReaderOpenNext();
+	public boolean hasNext() throws IOException, CollectionException {
+		BufferedReader br = getBufferedReader();
 
-    }
+		int readSeq = 0;
+		sequences = new ArrayList<List<String>>();
+		List<String> sequence = new ArrayList<>();
+		String readLine = null;
+		while ((readLine = br.readLine()) != null) {
+			if (readLine.isEmpty()) {
+				readSeq++;
+				if (readSeq == seqLimit) {
+					break;
+				}
+				sequences.add(sequence);
+				sequence = new ArrayList<>();
+				continue;
+			}
+			sequence.add(readLine);
+		}
+		if (!sequence.isEmpty()) {
+			sequences.add(sequence);
+		}
+		if (!sequences.isEmpty()) {
+			return true;
+		}
 
-    private boolean closeReaderOpenNext()
-        throws CollectionException, IOException
-    {
-        try {
-            bfs.get(currentReader).close();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
+		return closeReaderOpenNext();
 
-        if (currentReader + 1 < bfs.size()) {
-            currentReader++;
-            return hasNext();
-        }
-        return false;
-    }
+	}
 
-    private BufferedReader getBufferedReader()
-    {
-        return bfs.get(currentReader);
-    }
+	private boolean closeReaderOpenNext() throws CollectionException, IOException {
+		reader.close();
+		reader = null;
 
-    public Progress[] getProgress()
-    {
-        return null;
-    }
+		if (pointer + 1 < resourcePool.size()) {
+			pointer++;
+			Resource resource = resourcePool.get(pointer);
+			reader = new BufferedReader(openStream(resource));
+			return hasNext();
+		}
+		return false;
+	}
+
+	private InputStreamReader openStream(Resource resource) throws IOException {
+		String name = resource.getResource().getFile().getName();
+		InputStreamReader is = null;
+		if (name.endsWith(".gz")) {
+			is = new InputStreamReader(new GZIPInputStream(resource.getInputStream()), encoding);
+		} else {
+			is = new InputStreamReader(resource.getInputStream(), encoding);
+		}
+		return is;
+	}
+
+	private BufferedReader getBufferedReader() {
+		return reader;
+	}
+
+	public Progress[] getProgress() {
+		return null;
+	}
 }
